33687fa2ee31a580468b36239dca54c57ab3fc06
Nisharg Shah <nishargshah3101@gmail.com>
2022-11-25 00:03:01 +0530

    setup done

--- src/__mocks__/firebase.ts
+++ src/__mocks__/firebase.ts
@@ -2,13 +2,12 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
 export const firebaseApp = initializeApp({
-  apiKey: "AIzaSyAtg3bgA8xxBswNaikTlnGORfv3gBsynHk",
-  authDomain: "devfestahm-develop.firebaseapp.com",
-  databaseURL: "https://devfestahm-develop.firebaseio.com",
-  projectId: "devfestahm-develop",
-  storageBucket: "devfestahm-develop.appspot.com",
-  messagingSenderId: "439087005785",
-  appId: '1:439087005785:web:9e2e954d67ff7d3f397148',
-  measurementId: 'G-LH5M0DD9TK',  
+  apiKey: "AIzaSyAlGQM1tvp1tOzJzYMh-YLDNoQGp7VnHqE",
+  authDomain: "devfestahm-62cc4.firebaseapp.com",
+  databaseURL: "https://devfestahm-62cc4-default-rtdb.firebaseio.com",
+  projectId: "devfestahm-62cc4",
+  storageBucket: "devfestahm-62cc4.appspot.com",
+  messagingSenderId: "178324599142",
+  appId: "1:178324599142:web:db50e892c65e015013d8a7"
 });
 export const db = getFirestore(firebaseApp);
