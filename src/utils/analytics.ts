099fadc822a5bb715e04d03892b9d80b123c2225
Ravi Rupareliya <rupareliyaravi@gmail.com>
2024-10-26 15:22:08 +0530

    feat: added route name in firebase analytics

--- src/utils/analytics.ts
+++ src/utils/analytics.ts
@@ -1,5 +1,5 @@
 import { logEvent } from 'firebase/analytics';
 import { analytics } from '../firebase';
 
-export const logPageView = () => logEvent(analytics, 'page_view');
+export const logPageView = (pageName = 'page_view') => logEvent(analytics, pageName);
 export const logLogin = () => logEvent(analytics, 'login');
